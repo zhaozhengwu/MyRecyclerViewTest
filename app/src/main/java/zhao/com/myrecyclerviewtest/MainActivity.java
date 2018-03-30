864cf04df0211a6e1eb4c31ebf374b7be4abd4d8
zhaozhengwu <1366025523@qq.com>
2018-03-30 16:59:24 +0800

    ---

--- app/src/main/java/zhao/com/myrecyclerviewtest/MainActivity.java
+++ app/src/main/java/zhao/com/myrecyclerviewtest/MainActivity.java
@@ -1,11 +1,7 @@
 package zhao.com.myrecyclerviewtest;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
+
 
 import java.util.ArrayList;
 import java.util.List;
